d9dd58964e3a35d809d677d6d1ed930f42f1b942
Marcin <jozwiak1997@wp.pl>
2024-05-28 18:57:55 +0200

    Code refactor.

--- warehouse-service/warehouse-container/src/main/resources/data.sql
+++ warehouse-service/warehouse-container/src/main/resources/data.sql
@@ -2,17 +2,3 @@ INSERT INTO warehouse.warehouses(warehouse_id, name, available)
 	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb11', 'warehouse_1', TRUE);
 INSERT INTO warehouse.warehouses(warehouse_id, name, available)
 	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb14', 'warehouse_2', FALSE);
-
---INSERT INTO warehouse.products(id, name, price, available)
---	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb66', 'product_1', 25.00, FALSE);
---INSERT INTO warehouse.products(id, name, price, available)
---	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb67', 'product_2', 50.00, TRUE);
---INSERT INTO warehouse.products(id, name, price, available)
---	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb68', 'product_3', 20.00, FALSE);
---INSERT INTO warehouse.products(id, name, price, available)
---	VALUES ('1ecf862b-6395-412c-ba56-59d38e2764a5', 'product_4', 40.00, TRUE);
---
---INSERT INTO warehouse.warehouse_products(id, warehouse_id, product_id)
---	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb51', 'd215b5f8-0249-4dc5-89a3-51fd148cfb11', 'd215b5f8-0249-4dc5-89a3-51fd148cfb66');
---INSERT INTO warehouse.warehouse_products(id, warehouse_id, product_id)
---	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb54', 'd215b5f8-0249-4dc5-89a3-51fd148cfb14', '1ecf862b-6395-412c-ba56-59d38e2764a5');
