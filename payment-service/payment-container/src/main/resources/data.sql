ca83fa546f2fc82e85294a0f84c000bedfaf27f1
Marcin <jozwiak1997@wp.pl>
2024-05-28 19:01:12 +0200

    Code refactor.

--- payment-service/payment-container/src/main/resources/data.sql
+++ payment-service/payment-container/src/main/resources/data.sql
@@ -10,7 +10,6 @@ VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb24', 'd215b5f8-0249-4dc5-89a3-51fd148
 INSERT INTO payment.billfold_history(id, customer_id, amount, transaction_type)
 VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb25', 'd215b5f8-0249-4dc5-89a3-51fd148cfb41', 810.00, 'DEBIT');
 
-
 INSERT INTO payment.billfold(id, customer_id, total_billfold_amount)
 VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb22', 'd215b5f8-0249-4dc5-89a3-51fd148cfb43', 300.00);
 
